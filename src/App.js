6f57f269aaa07ffb4c7a70f477c2729558f416e5
Aniket <aniketsaste96@gmail.com>
2022-05-10 11:54:53 +0530

    initializing

--- src/App.js
+++ src/App.js
@@ -1,43 +1,5 @@
-import "./App.css";
-import { Button, Typography, styled } from "@mui/material";
-import { Settings, KeyboardArrowRight } from "@mui/icons-material";
-
 function App() {
-  const BlueButton = styled(Button)({
-    backgroundColor: "green",
-    margin: 5,
-    color: "white",
-    "&:hover": {
-      backgroundColor: "purple",
-    },
-  });
-  return (
-    <div>
-      <Button
-        startIcon={<KeyboardArrowRight />}
-        color="otherColor"
-        variant="contained"
-        size="small"
-      >
-        Text
-      </Button>
-      <Button
-        variant="contained"
-        color="secondary"
-        size="small"
-        startIcon={<Settings />}
-      >
-        Setting
-      </Button>
-      <Button variant="outlined" disabled>
-        Outlined
-      </Button>
-      <Typography variant="h1" component="p">
-        h1. Heading
-      </Typography>
-      <BlueButton>My Custom Button</BlueButton>
-    </div>
-  );
+  return <>Hello world</>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { theme } from "./theme.js"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
 
-    </ThemeProvider>
+    <App />
+
+
   </React.StrictMode>
 );
 
