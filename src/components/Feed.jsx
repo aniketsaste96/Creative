448b17d20af1434849823d2b41394825a3391cf9
Aniket <aniketsaste96@gmail.com>
2022-05-10 17:23:05 +0530

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,54 @@
-import { Box } from "@mui/material";
+import { ExpandMore, Favorite, MoreVert, Share } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React from "react";
-
+const image = [
+  "https://images.unsplash.com/photo-1642566111338-089143626b77?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=812&q=80",
+  "https://images.unsplash.com/photo-1652116759161-047b747091a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
+  "https://images.unsplash.com/photo-1652122203971-51cee22fc223?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+];
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4} p={2}>
-      Feed
+    <Box flex={4} p={2}>
+      {image.map((item) => (
+        <Card sx={{ maxWidth: "100%", mb: "30px" }}>
+          <CardHeader
+            avatar={<Avatar aria-label="recipe">R</Avatar>}
+            action={
+              <IconButton aria-label="settings">
+                <MoreVert />
+              </IconButton>
+            }
+            title="Gooderham Building at rainy day with the CN Tower in the background"
+            subheader="September 14, 2016"
+          />
+          <CardMedia component="img" height="300" image={item} />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to
+              cook together with your guests. Add 1 cup of frozen peas along
+              with the mussels, if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <Favorite />
+            </IconButton>
+            <IconButton aria-label="share">
+              <Share />
+            </IconButton>
+          </CardActions>
+        </Card>
+      ))}
     </Box>
   );
 };
